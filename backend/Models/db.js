8f985f9f0444198edca9d16fb58cfe7853fb410c
IqraKhanZ <iqrakhan30oct@gmail.com>
2026-03-23 01:01:01 +0530

    Updated db.js

--- backend/Models/db.js
+++ backend/Models/db.js
@@ -1,14 +1,38 @@
 const mongoose = require('mongoose');
 
+let cachedConnection = null;
+let connectionPromise = null;
+
 const connectDatabase = async () => {
+	if (cachedConnection || mongoose.connection.readyState === 1) {
+		cachedConnection = mongoose.connection;
+		return cachedConnection;
+	}
+
 	const mongoUri = process.env.MONGO_CONN;
 
 	if (!mongoUri) {
 		throw new Error('MONGO_CONN is not set in environment variables.');
 	}
 
-	await mongoose.connect(mongoUri);
-	console.log('MongoDB connected successfully.');
+	if (!connectionPromise) {
+		connectionPromise = mongoose
+			.connect(mongoUri, {
+				serverSelectionTimeoutMS: 30000,
+				maxPoolSize: 10
+			})
+			.then((connection) => {
+				cachedConnection = connection.connection;
+				console.log('MongoDB connected successfully.');
+				return cachedConnection;
+			})
+			.catch((error) => {
+				connectionPromise = null;
+				throw error;
+			});
+	}
+
+	return connectionPromise;
 };
 
 module.exports = connectDatabase;
